799cad6222bcb52d3a182f5939be45b1d441fb3e
fares <bommbala8@gmail.com>
2026-03-19 00:15:09 +0100

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -18,9 +18,7 @@ export default function Page(){
         <Features />
         <HowItWorks />
         <Pricing />
-        <div className="mt-16">
-          <Blog />
-        </div>
+        
         <div className="mt-16">
           <Testimonials />
         </div>
